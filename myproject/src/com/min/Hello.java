13630eb00bb57a2ab871f64604a6d90d80be2684
user <user@es302n28>
2018-09-25 12:14:55 +0800

    initial

---
+++ myproject/src/com/min/Hello.java
@@ -0,0 +1,18 @@
+package com.min;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 18;
+	    System.out.println(age);
+	    float height = (float)159.3;
+	    short number = (short)age;
+	    double weight = height;
+	    System.out.println(weight);
+	    System.out.println(number);
+	    char floor = 5;
+	    System.out.println(floor);
+	}
+
+}
